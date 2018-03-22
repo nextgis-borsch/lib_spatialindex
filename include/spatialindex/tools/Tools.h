2505f4cdce52aae8ba49d647d5c7d5b08be470fd
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-03-22 18:27:37 +0300

    Fix Windows build

--- include/spatialindex/tools/Tools.h
+++ include/spatialindex/tools/Tools.h
@@ -5,7 +5,7 @@
  * Copyright (c) 2004, Marios Hadjieleftheriou
  *
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
@@ -29,6 +29,7 @@
 
 
 #if (defined _WIN32 || defined _WIN64 || defined WIN32 || defined WIN64) && !defined __GNUC__
+#if _MSC_VER <= 1500
   typedef __int8 int8_t;
   typedef __int16 int16_t;
   typedef __int32 int32_t;
@@ -37,6 +38,7 @@
   typedef unsigned __int16 uint16_t;
   typedef unsigned __int32 uint32_t;
   typedef unsigned __int64 uint64_t;
+#endif
 
 // Nuke this annoying warning.  See http://www.unknownroad.com/rtfm/VisualStudio/warningC4251.html
 #pragma warning( disable: 4251 )

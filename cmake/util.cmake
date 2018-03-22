54fd7d4dc374d9cd3dc104de34fcc9f0811ceab2
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-03-22 12:27:01 +0300

    Update to borsch v2

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -50,52 +50,100 @@ function(check_version major minor rev lib sover)
 
     # Store version string in file for installer needs
     file(TIMESTAMP ${CHECK_FILE} VERSION_DATETIME "%Y-%m-%d %H:%M:%S" UTC)
-    file(WRITE ${CMAKE_BINARY_DIR}/version.str "${MAJOR_VERSION}.${MINOR_VERSION}.${REV_VERSION}\n${VERSION_DATETIME}")
+    set(VERSION ${MAJOR_VERSION}.${MINOR_VERSION}.${REV_VERSION})
+    get_cpack_filename(${VERSION} PROJECT_CPACK_FILENAME)
+    file(WRITE ${CMAKE_BINARY_DIR}/version.str "${VERSION}\n${VERSION_DATETIME}\n${PROJECT_CPACK_FILENAME}")
 
 endfunction(check_version)
 
 function(report_version name ver)
 
-    string(ASCII 27 Esc)
-    set(BoldYellow  "${Esc}[1;33m")
-    set(ColourReset "${Esc}[m")
+string(ASCII 27 Esc)
+set(BoldYellow  "${Esc}[1;33m")
+set(ColourReset "${Esc}[m")
 
-    message(STATUS "${BoldYellow}${name} version ${ver}${ColourReset}")
+message("${BoldYellow}${name} version ${ver}${ColourReset}")
 
 endfunction()
 
-
 # macro to find packages on the host OS
 macro( find_exthost_package )
-    if(CMAKE_CROSSCOMPILING)
-        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
-        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
-        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
+if(CMAKE_CROSSCOMPILING)
+    set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+    set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
+    set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
 
-        find_package( ${ARGN} )
+    find_package( ${ARGN} )
 
-        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
-        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
-        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
-    else()
-        find_package( ${ARGN} )
-    endif()
+    set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
+    set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+    set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+else()
+    find_package( ${ARGN} )
+endif()
 endmacro()
 
 
 # macro to find programs on the host OS
 macro( find_exthost_program )
-    if(CMAKE_CROSSCOMPILING)
-        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
-        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
-        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
+if(CMAKE_CROSSCOMPILING)
+    set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+    set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
+    set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
 
-        find_program( ${ARGN} )
+    find_program( ${ARGN} )
 
-        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
-        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
-        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
-    else()
-        find_program( ${ARGN} )
-    endif()
+    set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
+    set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+    set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+else()
+    find_program( ${ARGN} )
+endif()
 endmacro()
+
+# macro to find path on the host OS
+macro( find_exthost_path )
+if(CMAKE_CROSSCOMPILING)
+    set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+    set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
+    set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
+
+    find_path( ${ARGN} )
+
+    set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
+    set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+    set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+else()
+    find_path( ${ARGN} )
+endif()
+endmacro()
+
+function(get_cpack_filename ver name)
+get_compiler_version(COMPILER)
+if(BUILD_STATIC_LIBS)
+    set(STATIC_PREFIX "static-")
+endif()
+
+set(${name} ${PROJECT_NAME}-${STATIC_PREFIX}${ver}-${COMPILER} PARENT_SCOPE)
+endfunction()
+
+function(get_compiler_version ver)
+## Limit compiler version to 2 or 1 digits
+string(REPLACE "." ";" VERSION_LIST ${CMAKE_C_COMPILER_VERSION})
+list(LENGTH VERSION_LIST VERSION_LIST_LEN)
+if(VERSION_LIST_LEN GREATER 2 OR VERSION_LIST_LEN EQUAL 2)
+    list(GET VERSION_LIST 0 COMPILER_VERSION_MAJOR)
+    list(GET VERSION_LIST 1 COMPILER_VERSION_MINOR)
+    set(COMPILER ${CMAKE_C_COMPILER_ID}-${COMPILER_VERSION_MAJOR}.${COMPILER_VERSION_MINOR})
+else()
+    set(COMPILER ${CMAKE_C_COMPILER_ID}-${CMAKE_C_COMPILER_VERSION})
+endif()
+
+if(WIN32)
+    if(CMAKE_CL_64)
+        set(COMPILER "${COMPILER}-64bit")
+    endif()
+endif()
+
+set(${ver} ${COMPILER} PARENT_SCOPE)
+endfunction()
